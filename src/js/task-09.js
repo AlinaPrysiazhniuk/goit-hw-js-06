4b356f5ca1ce8749662b94719bc34528ea2a73f9
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-02-25 12:27:40 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
+const refs = {
+  spanEl: document.querySelector(".color"),
+  buttonEl: document.querySelector(".change-color"),
+  body: document.querySelector("body"),
+};
+
+refs.buttonEl.addEventListener("click", onInputChange);
+
+function onInputChange() {
+  const currentColor = getRandomHexColor();
+  refs.body.style.backgroundColor = currentColor;
+  refs.spanEl.textContent = currentColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
