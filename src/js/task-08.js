042770847b0cb4e9b8cabfe75fbf89f6eef5c60a
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-02-25 12:02:41 +0200

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const refs = {
+  formEl: document.querySelector(".login-form"),
+  emailEl: document.querySelector(".user-email"),
+  passwordEl: document.querySelector(".user-password"),
+};
 
+refs.formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (refs.emailEl.value === "" || refs.passwordEl.value === "") {
+    alert("Всі поля мають бути заповнені!");
+  }
+
+  if (refs.emailEl.value !== "" && refs.passwordEl.value) {
+    const userData = {
+      email: refs.emailEl.value,
+      password: refs.passwordEl.value,
+    };
+
+    console.log(userData);
+    refs.formEl.reset();
+  }
+});
