c1aac7af724c46ec1a718387e64dc27841c08f0f
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-02-26 15:42:00 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,38 +1,28 @@
-//записати у змінні елементи input та дві кнопки
-//додати на кнопки слухача події
-//Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
-//Розміри найпершого < div > - 30px на 30px.
-//Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
-
 const refs = {
   inputEl: document.querySelector("[data-number]"),
   buttonCreate: document.querySelector("[data-create]"),
   buttonDestroy: document.querySelector("[data-destroy]"),
-  divControl: document.querySelector("#controls"),
+  divEl: document.querySelector(".boxes"),
 };
 
 //-----------------створення колекції
 refs.buttonCreate.addEventListener("click", createBoxes);
 
-//-------------пишемо функцію, що рендерить колекцію елементів
-
-function createBoxes(amount) {
+function createBoxes() {
   let inputNumber = refs.inputEl.value;
-  console.log(inputNumber);
+  const arrayBoxes = [];
 
-  const newEl = document.createElement("div");
-  newEl.classList.add("boxes1");
-  newEl.style.width = "30px";
-  newEl.style.height = "30px";
-  newEl.style.color = getRandomHexColor();
-  console.log(newEl);
-  const colorEl = getRandomHexColor();
-  console.log(colorEl);
-  refs.divControl.insertAdjacentHTML("beforeEnd", newEl);
-  //refs.divControl.append(newEl1);
-  //return newEl;
-  //newEl.textContent = ingredient;
-  //return newItem;
+  for (let i = 0; i < inputNumber; i++) {
+    const box = document.createElement("div");
+    box.classList.add("boxes");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    arrayBoxes.push(box);
+  }
+
+  refs.divEl.append(...arrayBoxes);
+  console.log(arrayBoxes);
 }
 
 function getRandomHexColor() {
@@ -43,6 +33,8 @@ function getRandomHexColor() {
 
 //-----------------видалення колекції
 refs.buttonDestroy.addEventListener("click", destroyBoxes);
+
 function destroyBoxes() {
   refs.inputEl.value = "";
+  refs.divEl.innerHTML = "";
 }
